cf70e4720848a5a85808ac839ba4383b2d37fdf4
Bruno Silveira <silverdev42@gmail.com>
2022-12-30 01:00:45 -0300

    Created MVP Stable

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-function addNewEntry() {
-  if (formPreenchido() == false) {
-    alert('Por favor preencha os campos!');
-  }
-  if (formPreenchido() == true) {
-    let formData = getFormData();
-    console.log(formData);
-    createEntry(formData, "http://192.168.15.53:5000/entry", "POST");
-  }
+function addNewEntry(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  
+  const formJSON = Object.fromEntries(data.entries());
+  
+  let result = JSON.stringify(formJSON, null, 2);
+  sendRequest(result, "http://192.168.15.53:5000/entry", "POST");
 }
 
-const getFormData = () => {
-  if (document.getElementById('input-output').value === 'input') {
-    input_output = 1;
-  }
-  if (document.getElementById('input-output').value === 'output') {
-    input_output = 0;
-  }
+function addNewCategory(event) {
+  event.preventDefault();
 
-  return {
-    name: document.getElementById('name').value,
-    date: document.getElementById('date').value,
-    value: document.getElementById('value').value,
-    store: document.getElementById('store').value,
-    category: document.getElementById('categories').value,
-    description: document.getElementById('description').value,
-    input_output,
-  };
-};
-
-const getCategories = () => {
-	createEntry('', "http://192.168.15.53:5000/categories", "GET")
+  const data = new FormData(event.target);
+  
+  const formJSON = Object.fromEntries(data.entries());
+  
+  let result = JSON.stringify(formJSON, null, 2);
+  sendRequest(result, "http://192.168.15.53:5000/categories", "POST");
 }
 
-const createEntry = async (data, url, method) => {
-  const body = JSON.stringify(data); // body data type must match "Content-Type" header
+function addNewCategoryType(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  
+  const formJSON = Object.fromEntries(data.entries());
+  
+  let result = JSON.stringify(formJSON, null, 2);
+  sendRequest(result, "http://192.168.15.53:5000/category/types", "POST");
+}
+
+const entryForm = document.getElementById('entryForm');
+if (entryForm){
+  entryForm.addEventListener('submit', addNewEntry);
+}
+
+const categoryForm = document.getElementById('categoryForm');
+if (categoryForm){
+  categoryForm.addEventListener('submit', addNewCategory);
+}
+
+const categoryTypeForm = document.getElementById('categoryTypeForm');
+if (categoryTypeForm){
+  categoryTypeForm.addEventListener('submit', addNewCategoryType);
+}
+
+const sendRequest = async (body, url, method) => {
 	let options = '';
 
 	if (method === 'GET') {
@@ -62,25 +75,156 @@ const createEntry = async (data, url, method) => {
 		};
 
 	}
-
+  const results = document.getElementById('results');
   await fetch(url, options)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log('Success:', result);
+    .then((response) => {
+      response.json()
+    
+      .then((result) => {
+        if (response.status < 200 || response.status >= 300) {
+          results.innerHTML = 'Erro: ' + result.message;
+        }
+        else {
+          results.innerHTML = 'Sucesso!';
+        }
+      })
+      .catch((error) => {
+        results.innerHTML = 'Erro: ' + error;
+      });
+      
     })
     .catch((error) => {
-      console.error('Error:', error);
+      results.innerHTML = 'Erro: ' + error;
     });
-};
+}
 
-const formPreenchido = () => {
-  let validity = false;
-  document.forms['form'].addEventListener(
-    'submit',
-    () => {
-      validity = document.forms['form'].reportValidity();
-    },
-    false
-  );
-  return validity;
-};
+async function getCategories() {
+
+  const apiResponse = await fetch("http://192.168.15.53:5000/categories")
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+  
+  const select = document.getElementById('category');
+  for(var i=0; i < apiResponse.length; i++){
+    if (select) {
+      select.options[select.options.length] = new Option(apiResponse[i].name, apiResponse[i].categoryId);
+    } else {
+      alert("Where did the select go?");
+    }
+  }
+}
+
+async function getCategoryTypes() {
+
+  const apiResponse = await fetch("http://192.168.15.53:5000/category/types")
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+
+  const select = document.getElementById('categoryTypes');
+  for(var i=0; i < apiResponse.length; i++){
+    if (select) {
+      select.options[select.options.length] = new Option(apiResponse[i].name, apiResponse[i].typeId);
+    } else {
+      alert("Where did the select go?");
+    }
+  }
+}
+async function listCategories() {
+    
+  const apiResponse = await fetch("http://192.168.15.53:5000/categories")
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+
+  var tableDiv = document.createElement("table");
+  var prDiv = document.getElementById("pr");
+  
+  prDiv.innerHTML = "";
+  
+  var trElement =  document.createElement("tr");
+  tableDiv.appendChild(trElement);
+  
+  var tdElement =  document.createElement("td");
+  tdElement.innerHTML = "<b>" + "Nome da categoria" + "</b>";
+  trElement.appendChild(tdElement);
+
+  var tdElement =  document.createElement("td");
+  tdElement.innerHTML = "<b>" + "Tipo da categoria" + "</b>";
+  trElement.appendChild(tdElement);
+
+  apiResponse.forEach(function(a_column){
+    var trElement =  document.createElement("tr");
+    tableDiv.appendChild(trElement);
+    
+    var tdElement =  document.createElement("td");
+    tdElement.innerHTML = "<b>" + a_column.name + "</b>";
+    trElement.appendChild(tdElement);
+
+    var tdElement =  document.createElement("td");
+    tdElement.innerHTML = "<b>" + a_column.type + "</b>";
+    trElement.appendChild(tdElement);
+
+  });
+  prDiv.appendChild(tableDiv);
+}
+
+
+async function listCategoryTypes() {
+
+  const apiResponse = await fetch("http://192.168.15.53:5000/category/types")
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+
+  var tableDiv = document.createElement("table");
+  var prDiv = document.getElementById("pr");
+  
+  prDiv.innerHTML = "";
+
+  apiResponse.forEach(function(a_column){
+    var trElement =  document.createElement("tr");
+    tableDiv.appendChild(trElement);
+    
+    var tdElement =  document.createElement("td");
+    tdElement.innerHTML = "<b>" + a_column.name + "</b>";
+    trElement.appendChild(tdElement);
+
+  });
+  prDiv.appendChild(tableDiv);
+}
+
+
+async function getCategoriesBalance(type) {
+
+  const apiResponse = await fetch("http://192.168.15.53:5000/categories?type="+type)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+  
+  var tableDiv = document.createElement("table");
+  var prDiv = document.getElementById("pr");
+  
+  prDiv.innerHTML = "";
+
+  apiResponse.forEach(function(a_column){
+    var trElement =  document.createElement("tr");
+    tableDiv.appendChild(trElement);
+    
+    var tdElement =  document.createElement("td");
+    tdElement.innerHTML = "<b>" + a_column.name + "</b>";
+    trElement.appendChild(tdElement);
+
+    var tdElement = document.createElement("td");
+    tdElement.innerHTML = "<b>" + a_column.balance + "</b>";
+    trElement.appendChild( tdElement );
+
+  });
+  prDiv.appendChild(tableDiv);
+}
